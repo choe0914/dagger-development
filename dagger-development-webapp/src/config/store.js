df3740f3dcc9cb0b67fa9e436f33c3b31438a81c
Elizabeth Cho <elizabethcho@Elizabeths-MacBook-Pro.local>
2023-02-20 15:53:21 -0500

    Display Grid with row and col index number

---
+++ dagger-development-webapp/src/config/store.js
@@ -0,0 +1,13 @@
+import {createStore, combineReducers} from 'redux'
+import playerReducer from "../client/components/GamePlayer/reducer"
+
+const rootReducer = combineReducers({
+    player: playerReducer,
+})
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+)
+
+export default store;
