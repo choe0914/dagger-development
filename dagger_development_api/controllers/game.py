d780a8497d4320004cfc57b0b7067e5b98573718
Hunter McVey <mcvey.hunter1234@gmail.com>
2023-03-19 23:31:41 -0500

    Initial Commit for KB-16
    Initial implementation for the blueprints of the API routes and functions needed for the backend

---
+++ dagger_development_api/controllers/game.py
@@ -0,0 +1,19 @@
+from dagger_development_api.controllers import game_blueprint
+from flask_cors import cross_origin
+from dagger_development_api.extensions import db
+
+# All routes for game data and calculations (if needed)
+@cross_origin(supports_credentials=True)
+@game_blueprint.route('/game')
+
+@game_blueprint('/calc_mvmnt')
+def valid_invalid():
+    return {"message": "test"}
+
+@game_blueprint('/suggestion')
+def ask_users_suggestion():
+    return {"message": "test"}
+
+@game_blueprint('/accusation')
+def check_win():
+    return {"message": "test"}
