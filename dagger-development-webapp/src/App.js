85f38be199733021f1b7b98a70d95ced81adcc95
choe0914 <echo35@jh.edu>
2023-03-21 23:09:42 -0400

    [KB-10] Update app.js

--- dagger-development-webapp/src/App.js
+++ dagger-development-webapp/src/App.js
@@ -1,12 +1,29 @@
+import React,  { useState, createContext, useEffect } from 'react';
 import './App.css';
-import BoardTile from "./client/components/GameBoard/BoardTile"
+import GameBoard from './client/components/gameBoard'
+import { SelectSuspect }from './client/components/login'
+import { GameStateContext, SelectedSuspect } from './client/model/context'
+
 
 function App() {
+    const [canStart, setCanStart] = useState(false);
+    const [hidden, setHidden] = useState(true);
+    const [selectedSuspect, setSelectedSuspect] = useState('');
+    // TODO:: Connect to Flask server
+    // useEffect(() => {
+    //     fetch()
+    // })
+
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <>
+    <GameStateContext.Provider value={[canStart, setCanStart]}>
+        <SelectedSuspect.Provider value={[selectedSuspect, setSelectedSuspect]}>
+            {!canStart && <SelectSuspect />}
+            {canStart && <div class="board"> <GameBoard /></div>}       
+        </SelectedSuspect.Provider>
+    </GameStateContext.Provider>
+
+    </>);
 }
 
 export default App;
