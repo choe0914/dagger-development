589a780cb3643274e26b077170bb0000400bbcfb
Mofiadudebro <christianasanchez98@gmail.com>
2023-03-25 16:54:27 -0400

    Update Skeletal.js

--- clue-less/src/components/Skeletal.js
+++ clue-less/src/components/Skeletal.js
@@ -1,5 +1,10 @@
 import './Skeletal.css';
 import React, { useRef } from 'react';
+
+// const [username, setUsername] = useState('');
+
+let username = '';
+
 function UserName () {
     const launchButton = useRef();
     function handleSignInClick(event) {
@@ -7,8 +12,15 @@ function UserName () {
         setTimeout(() => {
             launchButton.current.style.transform = "translateY(-1px)";
         }, 100);
+        userAction();
         // return (<Board />)
     }
+
+    const userAction = async () => {
+        const response = await fetch('http://127.0.0.1:5000/user/'+username);
+        const myJson = await response.json(); //extract JSON from the http response
+      }
+
     return (
         <main className="username-body">
             <section className="username-panel">
@@ -19,7 +31,7 @@ function UserName () {
                         <p id="game-description">A slimmed down and fully digital version of the classic board game. 
                             Join or invite friends to solve the mystery of who killed Boden "Boddy" Black.</p>
                         <span id="username-info">Please enter a unique username to begin</span>
-                        <input type="text" id="uname" name="uname" placeholder='Username'></input>
+                        <input type="text" id="uname" name="uname" placeholder='Username' onBlur={k => {username = k.target.value;}}></input>
                         <button id="launch" ref={launchButton} onClick={handleSignInClick}>Continue</button>
                     </section>
                 </section>
