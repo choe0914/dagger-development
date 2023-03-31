3f58d39dd19e6135c0b6f2ca3248c94103f4dde4
Hunter McVey <mcvey.hunter1234@gmail.com>
2023-03-31 18:24:33 -0500

    Implemented Start Game Function (No json response yet)

--- dagger_development_api/utils/utils.py
+++ dagger_development_api/utils/utils.py
@@ -1,4 +1,7 @@
 # Put any utils functions in this file
+import random
+
+winningHand = []
 
 #Function for handling invalid movement
 def invalid_mvmnt(ex):
@@ -37,5 +40,34 @@ def load_game(ex):
     return
 
 #Function used at the start of the game to deal hands
-def start_game(ex):
-    return
+def start_game(totalPlayers):
+    totalCards = 18                                      #Total cards is 21 - the winning 3 = 18                         
+
+    roomIndex = [1,3,5,9,11,13,17,19,21]
+    cardIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]         #index the cards in an array to sort
+
+    weaponRooms = []
+    playerHands = [[]  for i in range(totalPlayers)]        #Create the empty player hands
+
+    for x in range(6):                                      #randomize the rooms
+        holdRoom = random.randint(0,8)
+        weaponRooms.append(roomIndex[holdRoom])
+        
+    winningHand = [random.randint(0,5), random.randint(6,11), random.randint(12,20)]    #randomize the winner first
+
+    cardIndex.pop(winningHand[2])       #remove selected cards
+    cardIndex.pop(winningHand[1])
+    cardIndex.pop(winningHand[0])
+
+    while(totalCards > 0):              #loop through all the cards
+        for x in range(len(playerHands)):
+            if(totalCards == 0):        #stop when there are none left
+
+                break
+            indexValue = random.randint(0,totalCards-1)     #get a random card, add it to a hand, and remove it from the deck
+            cardPicked = cardIndex[indexValue]
+            playerHands[x].append(cardPicked)
+            cardIndex.pop(indexValue)
+            totalCards -= 1
+        
+    return(winningHand, playerHands, weaponRooms)    #return the winning hand, player hands, and room orientation
