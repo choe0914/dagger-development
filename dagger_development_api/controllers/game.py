6df32d14dcc39767c8819d2dff9e3e91ac8306e8
Hunter McVey <mcvey.hunter1234@gmail.com>
2023-03-31 23:00:24 -0500

    Combined API route into utility function

--- dagger_development_api/controllers/game.py
+++ dagger_development_api/controllers/game.py
@@ -1,50 +1,17 @@
 from dagger_development_api.controllers import game_blueprint
 from flask_cors import cross_origin
-from sqlalchemy import select
-from sqlalchemy import update
 from dagger_development_api.utils import utils
-from dagger_development_api.extensions import db
-from dagger_development_api.model.user import User
-from dagger_development_api.model.game import Game
-from dagger_development_api.model.player_state import PlayerState
-from flask import jsonify
 
 # All routes for game data and calculations (if needed)
 @cross_origin(supports_credentials=True)
 @game_blueprint.route('/game')
 
 @game_blueprint.route('/game/start/<gameid>')
-def start_game(currentGame):
-    currentGame = 123
-    handRotation = 0
+def start_game(gameID):
+    
+    value = utils.start_game(gameID)
 
-    #Databse queries for the game we are working with and the player states in them
-    gameStmt = select(Game).where(Game.gameId == currentGame)       
-    gameResult = db.session.execute(gameStmt)
-    playerStmt = select(PlayerState).where(PlayerState.gameId == currentGame)
-    playerResult = db.session.execute(playerStmt)
-
-    #For the game that we find, send the player list and calculate the hands
-    for user_obj in gameResult.scalars():
-        playerList = user_obj.players
-        winningHand, playerHands, weaponLayout = utils.start_game(len(playerList))
-
-        updateStmt = update(Game).where(Game.gameId == currentGame).values(hand=winningHand)
-        db.session.update(updateStmt)
-        db.session.commit()
-        
-
-    #For each player give them one of the hands (current would be in order they joined)
-    for user_obj in playerResult.scalars():
-        if playerHands[handRotation] != None:
-            updateStmt = update(PlayerState).where(PlayerState.gameId == currentGame).values(hand=playerHands[handRotation])
-            db.session.update(updateStmt)
-            db.session.commit()
-            handRotation += 1
-
-    resp = jsonify(weaponLayout=weaponLayout, success=True)
-    resp.status_code = 200
-    return resp
+    return {"message": "test"}
 
 @game_blueprint.route('/game/cal_mvmnt')
 def calc_mvmnt():
--- dagger_development_api/utils/utils.py
+++ dagger_development_api/utils/utils.py
@@ -1,5 +1,12 @@
 # Put any utils functions in this file
 import random
+from sqlalchemy import select
+from sqlalchemy import update
+from sqlalchemy import and_
+from dagger_development_api.utils import utils
+from dagger_development_api.extensions import db
+from dagger_development_api.model.game import Game
+from dagger_development_api.model.player_state import PlayerState
 
 winningHand = []
 
@@ -40,34 +47,68 @@ def load_game(ex):
     return
 
 #Function used at the start of the game to deal hands
-def start_game(totalPlayers):
-    totalCards = 18                                      #Total cards is 21 - the winning 3 = 18                         
+def start_game(currentGame):
+    #Total cards is 21 - the winning 3 = 18
+    totalCards = 18
+    handRotation = 0                        
 
+    #index the cards in an array to sort
     roomIndex = [1,3,5,9,11,13,17,19,21]
-    cardIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]         #index the cards in an array to sort
+    cardIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 
     weaponRooms = []
-    playerHands = [[]  for x in range(totalPlayers)]        #Create the empty player hands
 
-    for x in range(6):                                      #randomize the rooms
-        holdRoom = random.randint(0,8)
-        weaponRooms.append(roomIndex[holdRoom])
-        
-    winningHand = [random.randint(0,5), random.randint(6,11), random.randint(12,20)]    #randomize the winner first
+    #Databse queries for the game we are working with and the player states in them
+    gameStmt = select(Game).where(Game.gameId == currentGame)       
+    gameResult = db.session.execute(gameStmt)
+    playerStmt = select(PlayerState).where(PlayerState.gameId == currentGame)
+    playerResult = db.session.execute(playerStmt)
 
-    cardIndex.pop(winningHand[2])       #remove selected cards
-    cardIndex.pop(winningHand[1])
-    cardIndex.pop(winningHand[0])
+    #For the game that we find then send the player list and calculate the hands
+    for user_obj in gameResult.scalars():
+        playerList = user_obj.players
 
-    while(totalCards > 0):              #loop through all the cards
-        for x in range(len(playerHands)):
-            if(totalCards == 0):        #stop when there are none left
+        #playerHands = [[]  for x in range(len(playerList))]
 
-                break
-            indexValue = random.randint(0,totalCards-1)     #get a random card, add it to a hand, and remove it from the deck
-            cardPicked = cardIndex[indexValue]
-            playerHands[x].append(cardPicked)
-            cardIndex.pop(indexValue)
-            totalCards -= 1
-        
-    return(winningHand, playerHands, weaponRooms)    #return the winning hand, player hands, and room orientation
+        #Randomize the weapon starting rooms
+        for x in range(6):                                      
+            holdRoom = random.randint(0,8)
+            weaponRooms.append(roomIndex[holdRoom])
+
+        #randomize the winner first
+        winningHand = [random.randint(0,5), random.randint(6,11), random.randint(12,20)]
+
+        #remove selected cards
+        cardIndex.pop(winningHand[2])
+        cardIndex.pop(winningHand[1])
+        cardIndex.pop(winningHand[0])
+
+        #loop through all the cards
+        while(totalCards > 0):
+            for x in range(len(playerList)):
+                #stop when there are none left
+                if(totalCards == 0):
+
+                    break
+
+                #get a random card, add it to a hand, and remove it from the deck
+                indexValue = random.randint(0,totalCards-1)
+                cardPicked = cardIndex[indexValue]
+                playerList[x].hand.append(cardPicked)
+                cardIndex.pop(indexValue)
+                totalCards -= 1
+
+        updateStmt = update(Game).where(Game.gameId == currentGame).values(hand=winningHand)
+        db.session.update(updateStmt)
+        db.session.commit()
+
+    #For each player give them one of the hands (current would be in order they joined)
+    for user_obj in playerResult.scalars():
+        if playerList[handRotation] != None:
+            updateStmt = update(PlayerState).where(and_(PlayerState.gameId == currentGame, PlayerState.userId == playerList[handRotation].userId)).values(hand=playerList[handRotation])
+            db.session.update(updateStmt)
+            db.session.commit()
+            handRotation += 1
+
+    #return the room orientation    
+    return(weaponRooms)
