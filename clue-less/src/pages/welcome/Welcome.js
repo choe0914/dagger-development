62190e9d079ff63e31ba85ca4631e233c8e7f9fc
choe0914 <echo35@jh.edu>
2023-04-06 21:50:53 -0400

    Validate username

--- clue-less/src/pages/welcome/Welcome.js
+++ clue-less/src/pages/welcome/Welcome.js
@@ -1,10 +1,25 @@
 import './Welcome.css';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useForm } from "react-hook-form";
+
 let username = '';
 
 function UserName() {
-
+    const [dbUserList, setUsers] = useState([]);
+    const { register, getValues, formState: { errors } } = useForm({ mode: "onChange" });
     let navigate = useNavigate();
+
+    useEffect(() => {
+        // Retrieve all user name from DB - user table 
+        fetch('http://127.0.0.1:5000/user/get_all')
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data);
+            });
+    }, []);
+
+
     function handleSignInClick(event) {
         var launchButton = document.getElementById("launch-game-select");
         launchButton.style.transform = "translateY(2px)";
@@ -30,7 +45,6 @@ function UserName() {
         navigate('/joinGame');
     }
 
-
     function checkNameField(event) {
         var launchButton = document.getElementById("launch-game-select");
         var fieldText = document.getElementById("uname");
@@ -53,7 +67,14 @@ function UserName() {
                         <p id="game-description">A slimmed down and fully digital version of the classic board game.
                             Join or invite friends to solve the mystery of who killed Boden "Boddy" Black.</p>
                         <span id="username-info">Please enter a unique username to begin</span>
-                        <input type="text" id="uname" name="uname" placeholder='Username' onKeyUp={checkNameField} onBlur={k => { username = k.target.value; }}></input>
+                        <input type="text" id="uname" name="uname" placeholder='Username'
+                            onKeyUp={checkNameField} onBlur={k => { username = k.target.value; }}
+                            {...register("uname", {
+                                required: true, minLength: 6, validate: value => !dbUserList.includes(value)
+                            })}></input>
+                        {errors.uname && errors.uname.type === "required" && (<p id="murdered" className="font-bold" >User name is required</p>)}
+                        {errors.uname && errors.uname.type === "minLength" && (<p id="murdered" className="font-bold" >Username minimum length is 6 characters</p>)}
+                        {errors.uname && errors.uname.type === "validate" && (<p id="murdered" className="font-bold" >Username not unique enough</p>)}
                         <button id="launch-game-select" onClick={handleSignInClick} onMouseEnter={buttonHoverA} onMouseLeave={buttonHoverB}>Continue</button>
                         <span id="next-info">Next - Join or Start New Game</span>
                     </section>
@@ -66,7 +87,7 @@ function UserName() {
             </section>
 
 
-        </main>
+        </main >
     );
 }
 
