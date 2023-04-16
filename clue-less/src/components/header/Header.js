7f74dedcefb95faf3e0ca328620104b019362111
choe0914 <echo35@jh.edu>
2023-04-15 20:18:39 -0400

    remove comments

--- clue-less/src/components/header/Header.js
+++ clue-less/src/components/header/Header.js
@@ -11,30 +11,10 @@ function Header() {
         console.log("here");
         if (navi) {
             navi.style.visibility = "hidden";
-
         }
-
-        // return (<Board />)
     }
 
-    // function startGame() {
-    //     fetch("http://localhost:5000/game/start/" + window.gameId, {
-    //         method: "GET", // *GET, POST, PUT, DELETE, etc.
-    //         mode: "cors", // no-cors, *cors, same-origin
-    //         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },// body data type must match "Content-Type" header
-    //     }).then((response) => { return response.json(); }).then((data) => {
-    //         console.log(data)
-    //         // Filter Current Hand 
-    //         let charInfo = data.gameInfo.players.filter(charId => charId.characterId === characterNumbersToIds[window.playerCharacter - 1])[0];
-    //         // TODO : why I can't save card info?
-    //         setCurrentHand(state => ({ ...state, hand: charInfo }));
-    //         // Filter Winning Hand 
-    //         setWinningHand(data.gameInfo.winningHand);
-    //     })
-    // }
+
     return (
         <header>
             <img className="logo" id="header-logo" src={logo} alt="Dagger Development"></img>
